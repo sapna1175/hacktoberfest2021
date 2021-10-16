79be5fcf7f92f62eb81105edbc52ac917c7f1e30
sapna1175 <56158354+sapna1175@users.noreply.github.com>
2021-10-16 10:04:21 +0530

    Add files via upload

---
+++ plotting_graph.m
@@ -0,0 +1,11 @@
+%% plotting
+t = [0:0.01:0.98];
+y1 = sin(2*pi*4*t); 
+plot(t,y1);
+y2 = cos(2*pi*4*t);
+hold on;  % "hold off" to turn off
+plot(t,y2,'r');
+xlabel('time');
+ylabel('value');
+legend('sin','cos');
+title('my plot');
